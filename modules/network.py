2ff90ec7e1eb712a70e3dd4071acb30e337ddf62
laksjdjf <aikasama@hotmail.co.jp>
2023-01-31 12:23:47 +0900

    Update network.py

--- modules/network.py
+++ modules/network.py
@@ -25,8 +25,8 @@ class ResNetBlock(nn.Module):
 
 #ResNetBlockを並べたネットワーク、channelとblock数を設定できる。
 class PolicyValueNetwork(nn.Module):
-    def __init__(self):
-        super(PolicyValueNetwork, self, channels=64, blocks=4, fcl=128).__init__()
+    def __init__(self, channels=64, blocks=4, fcl=128):
+        super(PolicyValueNetwork, self).__init__()
         self.conv1 = nn.Conv2d(2, channels, kernel_size=3, stride=1, padding=1)
         self.norm1 = nn.BatchNorm2d(channels)
 
