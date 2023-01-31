9b2063da5dfe6e76c7188ebf2bd9cf8e7bc32148
laksjdjf <aikasama@hotmail.co.jp>
2023-01-31 11:17:24 +0900

    Update network.py

--- modules/network.py
+++ modules/network.py
@@ -1,6 +1,9 @@
+#ref:https://github.com/TadaoYamaoka/python-dlshogi2/blob/main/pydlshogi2/network/policy_value_resnet.py
+
 import torch
 import torch.nn as nn
 
+#ResNetBlock:dlshogiとほぼ同等の構造。
 class ResNetBlock(nn.Module):
     def __init__(self, channels):
         super(ResNetBlock, self).__init__()
@@ -20,30 +23,25 @@ class ResNetBlock(nn.Module):
         return torch.relu(out + x)
 
 
-
+#ResNetBlockを並べたネットワーク、channelとblock数を設定できる。
 class PolicyValueNetwork(nn.Module):
     def __init__(self):
-        super(PolicyValueNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(2, 64, kernel_size=3, stride=1, padding=1)
-        self.norm1 = nn.BatchNorm2d(64)
+        super(PolicyValueNetwork, self, channels=64, blocks=4, fcl=128).__init__()
+        self.conv1 = nn.Conv2d(2, channels, kernel_size=3, stride=1, padding=1)
+        self.norm1 = nn.BatchNorm2d(channels)
 
-        self.block1 = ResNetBlock(64)
-        self.block2 = ResNetBlock(64)
-        self.block3 = ResNetBlock(64)
-        self.block4 = ResNetBlock(64)
+        self.blocks = torch.nn.Sequential(*[ResNetBlock(channels) for _ in range(blocks)])
 
-        self.p1 = nn.Conv2d(64,1,kernel_size=1,stride=1,padding=0)
+        self.p1 = nn.Conv2d(channels,1,kernel_size=1,stride=1,padding=0)
 
-        self.v1 = nn.Linear(64*64, 128)
-        self.v2 = nn.Linear(128, 1)
+        self.v1 = nn.Linear(channels*64, fcl)
+        self.v2 = nn.Linear(fcl, 1)
 
     def forward(self, x):
         x = self.norm1(self.conv1(x))
         x = torch.relu(x)
-        x = self.block1(x)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
+        
+        x = self.blocks(x)
 
         policy = self.p1(x)
         policy = torch.flatten(policy,1)
